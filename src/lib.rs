50feb10e5a322df0f5e5ccf7cad8b9510d2d3c3b
Marc Rijken <marc@rijken.org>
2025-01-04 00:06:56 +0000

    fix doctest

--- src/lib.rs
+++ src/lib.rs
@@ -77,93 +77,80 @@ pub enum MatchingLayer {
 ///
 /// Typical usages:
 ///
-/// ## Omitting content that isn't targeted
-/// <https://unpoly.com/optimizing-responses#omitting-content-that-isnt-targeted>
+///```
+/// use axum::response::IntoResponse;
+/// use axum::extract;
+/// use serde::Deserialize;
+/// use serde_json::json;
 ///
-/// ```
-/// fn handler(unpoly: unpoly::Unpoly) -> impl IntoResponse {
+/// ///  Omitting content that isn't targeted
+/// /// https://unpoly.com/optimizing-responses#omitting-content-that-isnt-targeted
+/// fn handler_target(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
 ///     let target = unpoly.target();
 ///     let html = todo!("render content for target only");
 ///     (unpoly.get_headers().unwrap(), html)
 /// }
-/// ```
-/// ## Rendering different content for overlays
-/// <https://unpoly.com/optimizing-responses#rendering-different-content-for-overlays>
 ///
-/// ```
-/// fn handler(unpoly: unpoly::Unpoly) -> impl IntoResponse {
+/// ///  Rendering different content for overlays
+/// /// https://unpoly.com/optimizing-responses#rendering-different-content-for-overlays
+/// fn handler_mode_target(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
 ///     let mode = unpoly.mode();
 ///     let target = unpoly.target();
 ///     let html = todo!("render content for target in mode only");
 ///     (unpoly.get_headers().unwrap(), html)
 /// }
 ///
-/// ```
-///
-/// ## Rendering different content for unpoly requests
-/// <https://unpoly.com/optimizing-responses#rendering-different-content-for-unpoly-requests>
-///
-/// ```
-/// fn handler(unpoly: unpoly::Unpoly) -> impl IntoResponse {
-///     let html = if unpoly.is_up() {
-///         todo!("render for fragment update");
+/// /// Rendering different content for unpoly requests
+/// /// https://unpoly.com/optimizing-responses#rendering-different-content-for-unpoly-requests
+/// fn handler_full_or_fragment(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
+///     let html: String = if unpoly.is_up() {
+///         todo!("render for fragment update")
 ///     } else {
-///         todo!("render for full page load");
-///     }
+///         todo!("render for full page load")
+///     };
 ///     (unpoly.get_headers().unwrap(), html)
 /// }
-/// ```
 ///
-/// ## Rendering content that depends on layer context
-/// <https://unpoly.com/optimizing-responses#rendering-content-that-depends-on-layer-context>
-///
-/// ```
-/// fn handler(unpoly: unpoly::Unpoly) -> impl IntoResponse {
+/// /// Rendering content that depends on layer context
+/// /// https://unpoly.com/optimizing-responses#rendering-content-that-depends-on-layer-context
+/// fn handler_context(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
 ///     let context = unpoly.context();
 ///     let html = todo!("render html for context");
 ///     (unpoly.get_headers().unwrap(), html)
 /// }
-/// ```
 ///
-/// ## Set the title of the page via a fragment update
-/// ```
-/// fn handler(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
+/// /// Set the title of the page via a fragment update
+/// fn handler_title(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
 ///     unpoly.set_title("My App");
 ///     let html = todo!();
 ///     (unpoly.get_headers().unwrap(), html)
 /// }
-/// ```
 ///
-/// ## Send events to the frontend
-/// ```
-/// fn handler(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
+/// /// Send events to the frontend
+/// fn handler_emit_event(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
 ///     unpoly.emit_event("user:created", json!({"id": 152}));
 ///     // or for a specific layer
-///     unpoly.emit_event_layer("user:created", json!({"id": 152}), layer:MatchingLayer::CURRENT);
+///     unpoly.emit_event_layer("user:created", json!({"id": 152}), unpoly::MatchingLayer::CURRENT);
 ///     let html = todo!();
 ///     (unpoly.get_headers().unwrap(), html)
 /// }
-/// ```
 ///
-/// ## Expire cache
-/// ```
-/// fn handler(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
+/// /// Expire cache
+/// fn handler_cache(mut unpoly: unpoly::Unpoly) -> impl IntoResponse {
 ///     unpoly.set_expire_cache("/path/to/expire/*");
 ///     let html = todo!();
 ///     (unpoly.get_headers().unwrap(), html)
 /// }
-/// ```
 ///
 ///
-/// ## Validating a form
-/// <https://unpoly.com/up-validate>
-/// ```
+/// /// Validating a form
+/// /// https://unpoly.com/up-validate
 /// #[derive(Deserialize)]
 ///     struct SampleForm{
 ///     name: String,
 ///     email: String,
 /// }
-/// fn handler(mut unpoly: unpoly::Unpoly, extract::Form(form): extract::Form<SampleForm>) -> impl IntoResponse {
+/// fn handler_validate(mut unpoly: unpoly::Unpoly, extract::Form(form): extract::Form<SampleForm>) -> impl IntoResponse {
 ///     if !unpoly.validate().is_empty() {
 ///         todo!("Validate form");
 ///         let html = todo!("render form with optional errors");
@@ -388,12 +375,12 @@ impl Unpoly {
         Ok(())
     }
 
-    pub fn set_evict_cache(&mut self, cache: String) {
-        self.response_evict_cache = Some(cache);
+    pub fn set_evict_cache(&mut self, cache: impl Into<String>) {
+        self.response_evict_cache = Some(cache.into());
     }
 
-    pub fn set_expire_cache(&mut self, cache: String) {
-        self.response_expire_cache = Some(cache);
+    pub fn set_expire_cache(&mut self, cache: impl Into<String>) {
+        self.response_expire_cache = Some(cache.into());
     }
 
     pub fn get_headers(&self) -> Result<HeaderMap, Error> {
